7e0a5c60d814689d4d800d6ebd9e45dff1f6bdb0
Daniel-Osei <demajesty.6@gmail.com>
2020-07-01 00:40:43 +0000

    hmm, have to go back

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var path =  require('path');
-var open = require('open');
+// var express = require('express');
+import express from 'express';
+// var path =  require('path');
+import path from 'path';
+// var open = require('open');
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+// var port = 3000;
+const port = 3000;
+// var app = express();
+const app = express();
+const compiler = webpack(config);
 
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'))
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+// var chalk = require('chalk');  <=ES5
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode'));
